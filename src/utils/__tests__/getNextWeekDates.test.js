cd105b100cea292bd8af03365b15d4fc77d4607b
Fayeez Ahmed <fayeezahmed@gmail.com>
2022-04-24 18:42:33 +0100

    fixed unit test - huzzah

--- src/utils/__tests__/getNextWeekDates.test.js
+++ src/utils/__tests__/getNextWeekDates.test.js
@@ -19,7 +19,7 @@ describe("getNextWeekDates", () => {
 
   it("should populate next week's dates for any day this week if they span multiple months", () => {
     const expectedDays = [28, 29, 30, 31, 1, 2, 3];
-    const expectedMonth = 2
+    const expectedMonth = 3
     const expectedYear = 2022
     const thisWeek =  [21, 22, 23, 24, 25, 26, 27];
 
