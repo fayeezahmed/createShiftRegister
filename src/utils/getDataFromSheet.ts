0f00223071f9dce9ccc1aa743835a78662431c87
Fayeez Ahmed <fayeezahmed@gmail.com>
2022-04-26 19:25:20 +0100

    updated date to be in date format

--- src/utils/getDataFromSheet.ts
+++ src/utils/getDataFromSheet.ts
@@ -1,5 +1,5 @@
 function getData() {
-    const activeSheet = SpreadsheetApp.getActiveSheet();
+    const activeSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Chat Agents');
     const lastRow = activeSheet.getLastRow()
     const notationQuery = `Chat Agents!A2:D${lastRow}`
     const data = activeSheet.getRange(notationQuery)
