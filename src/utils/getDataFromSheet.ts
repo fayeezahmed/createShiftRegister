e01aae1dafcbc6d1aff1116c015f9f76f0f6ab93
Fayeez Ahmed <fayeezahmed@gmail.com>
2022-04-26 19:29:54 +0100

    ignore ts checker here

--- src/utils/getDataFromSheet.ts
+++ src/utils/getDataFromSheet.ts
@@ -1,7 +1,9 @@
 function getData() {
     const activeSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Chat Agents');
+    // @ts-ignore
     const lastRow = activeSheet.getLastRow()
     const notationQuery = `Chat Agents!A2:D${lastRow}`
+    // @ts-ignore
     const data = activeSheet.getRange(notationQuery)
 
     return data.getValues()
