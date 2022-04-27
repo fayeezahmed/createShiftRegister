c9286b5795552a0f5eacc57e1ecd47311d158e48
Fayeez Ahmed <fayeezahmed@gmail.com>
2022-04-27 15:48:30 +0100

    fix entering of incorrect day and cell

--- src/index.ts
+++ src/index.ts
@@ -28,8 +28,8 @@ function setupDays() {
   const nextWeekDates  = getNextWeekDates(new Date());
   nextWeekDates.forEach(date => {
     const day = new Date(date)
-    const formattedDay = Utilities.formatDate(day, "GMT", "EEEE d");
-    const dayName = day.toLocaleString('en-GB', { weekday: 'long' })
+    const dayName = day.toLocaleString('en-GB', { weekday: 'long' });
+    const formattedDay = `${dayName} ${day.getDate()}`
     activeSheet.getRange(`Weekly Schedule!${dayCellMap[dayName]}`).setValue(formattedDay)
   })
 }
